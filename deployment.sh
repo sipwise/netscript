273ad852a49d9729307f9f7dbbf00218c5cc2f1d
Andrew Pogrebennyk <apogrebennyk@sipwise.com>
2012-12-21 15:58:48 +0000

    final update for 2.7.

--- deployment.sh
+++ deployment.sh
@@ -570,6 +570,7 @@ if "$PRO_EDITION" ; then
     2.6) INSTALLER_VERSION="0.8.3";;
     2.7-rc2) INSTALLER_VERSION="0.9.0";;
     2.7-rc3) INSTALLER_VERSION="0.9.1";;
+    2.7) INSTALLER_VERSION="0.9.2";;
   esac
 elif "$CE_EDITION" ; then
   case "$SP_VERSION" in
@@ -583,6 +584,7 @@ elif "$CE_EDITION" ; then
     2.6) INSTALLER_VERSION="0.8.3";;
     2.7-rc2) INSTALLER_VERSION="0.9.0";;
     2.7-rc3) INSTALLER_VERSION="0.9.1";;
+    2.7) INSTALLER_VERSION="0.9.2";;
   esac
 fi
 
