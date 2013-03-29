147ff5145e06b8951af66ce1dce133a228e2a4be
Andreas Granig <agranig@sipwise.com>
2013-03-29 11:36:47 +0000

    Add 2.8 release and deprecate releases older than 2.4

--- deployment.sh
+++ deployment.sh
@@ -578,6 +578,7 @@ if "$PRO_EDITION" ; then
     2.7) INSTALLER_VERSION="0.9.2";;
     2.8-rc2) INSTALLER_VERSION="0.10.0";;
     2.8-rc3) INSTALLER_VERSION="0.10.1";;
+    2.8) INSTALLER_VERSION="0.10.2";;
   esac
 elif "$CE_EDITION" ; then
   case "$SP_VERSION" in
@@ -592,6 +593,7 @@ elif "$CE_EDITION" ; then
     2.7-rc2) INSTALLER_VERSION="0.9.0";;
     2.7-rc3) INSTALLER_VERSION="0.9.1";;
     2.7) INSTALLER_VERSION="0.9.2";;
+    2.8) INSTALLER_VERSION="0.10.2";;
   esac
 fi
 
