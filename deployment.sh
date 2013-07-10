4786ebd86f27c742a04429483cb95faee70976c5
Michael Prokop <mprokop@sipwise.com>
2013-07-10 11:09:27 +0200

    Stop glusterfs-server and monit processes
    Otherwise we've open file handles on /mnt

--- deployment.sh
+++ deployment.sh
@@ -1630,7 +1630,7 @@ fi
 
 # make sure we don't leave any running processes
 for i in asterisk atd collectd collectdmon dbus-daemon exim4 \
-         glusterfs glusterfsd haveged nscd   \
+         glusterfs glusterfsd glusterfs-server haveged monit nscd \
 	 redis-server snmpd voisniff-ng ; do
   killall -9 $i >/dev/null 2>&1 || true
 done
