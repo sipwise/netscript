120ad3c3721bf8cd7ba472846774ea4484f3ce0e
Andrew Pogrebennyk <apogrebennyk@sipwise.com>
2013-08-08 23:05:20 +0200

    Use new installer 0.11.3 for 3.0-rc2.

--- deployment.sh
+++ deployment.sh
@@ -544,7 +544,7 @@ if "$PRO_EDITION" ; then
     2.8-rc3) INSTALLER_VERSION="0.10.1";;
     2.8) INSTALLER_VERSION="0.10.2";;
     3.0-rc1) INSTALLER_VERSION="0.11.0";;
-    3.0-rc2) INSTALLER_VERSION="0.11.2";;
+    3.0-rc2) INSTALLER_VERSION="0.11.3";;
   esac
 elif "$CE_EDITION" ; then
   case "$SP_VERSION" in
@@ -561,7 +561,7 @@ elif "$CE_EDITION" ; then
     2.7)     INSTALLER_VERSION="0.9.2" ;  DEBIAN_RELEASE="squeeze" ;;
     2.8)     INSTALLER_VERSION="0.10.2" ; DEBIAN_RELEASE="squeeze" ;;
     3.0-rc1) INSTALLER_VERSION="0.11.0" ; DEBIAN_RELEASE="wheezy" ;;
-    3.0-rc2) INSTALLER_VERSION="0.11.2" ; DEBIAN_RELEASE="wheezy" ;;
+    3.0-rc2) INSTALLER_VERSION="0.11.3" ; DEBIAN_RELEASE="wheezy" ;;
   esac
 fi
 
