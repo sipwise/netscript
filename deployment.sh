sipwise/netscript

commit 05c83c88227ebcdac4bb15f4dd7ed2a5af59c170

MT#7335 Use our own Debian/jessie mirror
Nowadays we have our own Debian/jessie mirror, so no need to
use the external service any longer.

Change-Id: I7417680eab43efccb08d98d5b50dafa553296d3e

deployment.sh
@@ -1111,18 +1111,12 @@ cp /etc/apt/trusted.gpg.d/sipwise.gpg "\${MNTPOINT}"/etc/apt/trusted.gpg.d/
 EOF
 chmod 775 /etc/debootstrap/pre-scripts/install-sipwise-key.sh
 
-# drop this once we mirror Debian/jessie
-if [ "$DEBIAN_RELEASE" = "jessie" ] ; then
-  MIRROR='http://debian.inode.at/debian/'
-  KEYRING='/usr/share/keyrings/debian-archive-keyring.gpg'
-else
-  # NOTE: we use the debian.sipwise.com CNAME by intention here
-  # to avoid conflicts with apt-pinning, preferring deb.sipwise.com
-  # over official Debian
-  MIRROR="http://${DEBIAN_REPO_HOST}/debian/"
-  SEC_MIRROR="http://${DEBIAN_REPO_HOST}/debian-security/"
-  KEYRING='/etc/apt/trusted.gpg.d/sipwise.gpg'
-fi
+# NOTE: we use the debian.sipwise.com CNAME by intention here
+# to avoid conflicts with apt-pinning, preferring deb.sipwise.com
+# over official Debian
+MIRROR="http://${DEBIAN_REPO_HOST}/debian/"
+SEC_MIRROR="http://${DEBIAN_REPO_HOST}/debian-security/"
+KEYRING='/etc/apt/trusted.gpg.d/sipwise.gpg'
 
 set_deploy_status "debootstrap"
 

commit f40a15b788d02a3d5a0072a44ec40ba7f1500f9b

MT#7335 Disable security mirror when installing jessie
Noticed in failing Jenkins job
https://jenkins.mgm.sipwise.com/job/daily-build-install-vm/7470/
being downstream of https://jenkins.mgm.sipwise.com/job/build-trunk-systemd-vms:

| W: Failed to fetch http://debian.sipwise.com/debian-security/dists/jessie-security/non-free/binary-amd64/Packages  404  Not Found [IP: 77.244.249.93 80]

Debian/jessie gets security support once it's released as stable,
until then we've to ignore the (unavailable) debian-security mirror.

This fixes behaviour which got broken in git commit 05c83c88.

Change-Id: Icb5d9974b50119254f4ba0fa5b60b18a38dcb21c

deployment.sh
@@ -1128,7 +1128,6 @@ fi
 # to avoid conflicts with apt-pinning, preferring deb.sipwise.com
 # over official Debian
 MIRROR="http://${DEBIAN_REPO_HOST}/debian/"
-SEC_MIRROR="http://${DEBIAN_REPO_HOST}/debian-security/"
 KEYRING='/etc/apt/trusted.gpg.d/sipwise.gpg'
 
 set_deploy_status "debootstrap"
@@ -1140,12 +1139,13 @@ cat > /etc/debootstrap/etc/apt/sources.list << EOF
 deb ${MIRROR} ${DEBIAN_RELEASE} main contrib non-free
 EOF
 
-# drop this once Debian/jessie has security support
-if [ -n "$SEC_MIRROR" ] ; then
-  echo "deb ${SEC_MIRROR} ${DEBIAN_RELEASE}-security main contrib non-free" >> /etc/debootstrap/etc/apt/sources.list
+# drop this once Debian/jessie has security support (AKA released as stable)
+if [ "$DEBIAN_RELEASE" = "jessie" ] ; then
+  echo  "Warning: not enabling security repository for $DEBIAN_RELEASE"
+  logit "Warning: not enabling security repository for $DEBIAN_RELEASE"
 else
-  echo  "Warning: security mirror variable SEC_MIRROR is unset, not enabling security repository for $DEBIAN_RELEASE"
-  logit "Warning: security mirror variable SEC_MIRROR is unset, not enabling security repository for $DEBIAN_RELEASE"
+  SEC_MIRROR="http://${DEBIAN_REPO_HOST}/debian-security/"
+  echo "deb ${SEC_MIRROR} ${DEBIAN_RELEASE}-security main contrib non-free" >> /etc/debootstrap/etc/apt/sources.list
 fi
 
 echo "deb ${MIRROR} ${DEBIAN_RELEASE}-updates main contrib non-free" >> /etc/debootstrap/etc/apt/sources.list